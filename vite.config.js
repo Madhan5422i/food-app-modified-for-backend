76f8857df74810f7e93ccf8be44a734c099d2791
Madhan5422i <sathyakannan3085@gmail.com>
2024-05-28 22:49:08 +0530

    chore: update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  base:"food-app-modified-for-backend-/",
   plugins: [react()],
   esbuild: {
     jsxFactory: 'React.createElement',
